0ea1bdeb3dbbf9bf8879003c5f8ea9c749899e11
Kouhei <hiyotihakou@gmail.com>
2020-09-04 22:27:10 +0900

    rubocop error の修正

--- spec/models/article_like_spec.rb
+++ spec/models/article_like_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ArticleLike, type: :model do
   context "必要なカラムが指定されている時" do
     let(:article_like) { build(:article_like) }
-    fit "値が入力される" do
+    it "値が入力される" do
       expect(article_like).to be_valid
     end
   end
