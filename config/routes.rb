440bd9ae037f0fd0e1fcf698b9a11d2f152e549d
Kouhei <hiyotihakou@gmail.com>
2020-09-10 23:10:41 +0900

    フロントエンドのつなぎ込みの実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/index'
+  root to: "home#index"
+
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
   end
